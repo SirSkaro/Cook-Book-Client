91ac69f0f566bbaa3b37d6cfe9cabd4dd0a9426f
unknown <bencbchurchill@gmail.com>
2020-12-12 17:06:43 -0700

    Added confirmation modal for deleting a tag

--- src/views/ManageRecipeTags.vue
+++ src/views/ManageRecipeTags.vue
@@ -10,10 +10,9 @@
     <b-row>
       <b-table striped hover fixed :items="tags" :fields="tableFields">
         <template #cell(actions)="row">
-          <b-button size="sm" variant="info" v-b-modal.tag-modal @click="setupModalEdit(row.item)"><b-icon-pencil></b-icon-pencil></b-button>
-          <b-button size="sm" variant="danger" @click="deleteTag(row.item)"><b-icon-trash></b-icon-trash></b-button>
+          <b-button size="sm" variant="info" v-b-modal.tag-modal @click="setupModalModle(row.item)"><b-icon-pencil></b-icon-pencil></b-button>
+          <b-button size="sm" variant="danger" v-b-modal.delete-confirmation-modal @click="setupModalModle(row.item)"><b-icon-trash></b-icon-trash></b-button>
         </template>
-
       </b-table>
     </b-row>
 
@@ -28,13 +27,17 @@
       </b-col>
     </b-row>
 
-    <b-modal id="tag-modal" title="Add Recipe Tag" @ok="persistTag" :ok-disabled="!canSubmit">
+    <b-modal id="tag-modal" title="Edit Recipe Tag" @ok="saveTag" :ok-disabled="!canSubmit">
       <form>
         <b-form-group label="Label" label-for="label-input">
           <b-form-input id="label-input" v-model="$v.form.label.$model" aria-describedby="label-feedback" :state="validateState('label')"></b-form-input>
           <b-form-invalid-feedback id="label-feedback">A label is required</b-form-invalid-feedback>
         </b-form-group>
       </form>
+    </b-modal>
+    <b-modal id="delete-confirmation-modal" title="Delete Recipe Tag" @ok="deleteTag" ok-variant="danger" ok-title="Yes, delete">
+      <p>You are about to delete this tag. It will no longer be associated with any of your recipes.</p>
+      <p>Are you sure you want to delete this tag?</p>
     </b-modal>
   </b-container>
   
@@ -92,27 +95,15 @@ export default {
             this.pageConfig.totalItems = tagsPage.page.totalElements
           })
     },
-    persistTag() {
-      if(this.form._links) {
-        this.updateTag()
-      } else {
-        this.createTag()
-      }
-    },
-    updateTag() {
-      return TagsService.update(this.form)
+    saveTag() {
+      return TagsService.save(this.form)
         .then(this.loadTags)
     },
-    createTag() {
-      let tag = {label: this.form.label}
-      return TagsService.create(tag)
+    deleteTag() {
+      TagsService.delete(this.form)
         .then(this.loadTags)
     },
-    deleteTag(tag) {
-      TagsService.delete(tag)
-        .then(this.loadTags)
-    },
-    setupModalEdit(tag) {
+    setupModalModle(tag) {
       this.form = JSON.parse(JSON.stringify(tag))
       this.$v.form.$reset()
     },
