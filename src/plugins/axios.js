ad6f0f2e711963802aebe2e88a4345647a496fc9
unknown <bencbchurchill@gmail.com>
2021-12-19 19:50:44 -0800

    Added authoritzation header to all axios requests

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,3 +1,10 @@
 import Axios from 'axios';
+import UserService from '../services/UserService'
 
 Axios.defaults.baseURL = process.env.VUE_APP_BASE_URL;
+
+
+let sessionToken = UserService.getSessionToken()
+Axios.defaults.headers.common['Authorization'] = sessionToken 
+  ? 'Bearer ' + sessionToken
+  : null;
